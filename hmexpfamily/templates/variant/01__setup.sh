108537931fd8d1a83fad2d938be1b624942a29db
Leif Denby <lcd@dmi.dk>
2023-06-02 16:51:00 +0000

    more symlinks

--- hmexpfamily/templates/variant/01__setup.sh
+++ hmexpfamily/templates/variant/01__setup.sh
@@ -1,11 +1,21 @@
 {% with variant_identifier="variant__" + variant.name %}
+# Create a directory to store the {{ variant.name }} variant of the
+# {{ base.name }} experiment
 mkdir {{ variant_identifier }}
 
-# Symlink the "variant" experiment into hm_home on $SCRATCH where Harmone expects it
+# Setup Harmonie in $PERM with this variant experiment by symlinking to the
+# base experiment repo
+cd {{ variant_identifier }}
+ln -sf $PERM/hm_home/{{ base.name }}__base/config-sh/Harmonie
+./Harmonie setup -r $PERM/hm_home/{{ base.name }}__base
+cd -
+
+# Symlink the variant experiment into hm_home on $SCRATCH where Harmone expects it
 # to be
 ln -s {{ cwd }}/{{ variant_identifier }} $PERM/hm_home/{{ base.name }}__{{ variant_identifier }}
 
-## Create a symlink to where Harmonie will place the output
+# Create a symlink in output/{{ variant_identifier }} to where Harmonie will
+# place the output for ease of access later
 ln -s $SCRATCH/hm_home/{{ base.name }}__{{ variant_identifier }} {{ cwd }}/output/{{ variant_identifier }}
 
 {% endwith %}
